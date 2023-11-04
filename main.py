bd2cd5e2b2d6fbee10f17685cb33fccf776b510e
duydl <duy.dole.00ece@gmail.com>
2023-11-04 17:04:27 +0900

    fix: retry if fail

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ import quopri
 import re
 import requests
 import os
-from urllib.parse import unquote, urlparse, parse_qs
 import time
+from urllib.parse import unquote, urlparse, parse_qs
+
 
 href_pattern = r'href="([^"]*)"'
 # Path to your Thunderbird Local Folders
-local_folders_path = "C:/Users/onepi/AppData/Roaming/Thunderbird/Profiles/5wpb27lz.default-release/Mail/Local Folders/Kindle Notebook"
-output_folder = "C:/Projects"
+local_folders_path = "C:\\Users\\onepi\\AppData\\Roaming\\Thunderbird\\Profiles\\5wpb27lz.default-release\\Mail\\Local Folders\\Kindle Notebook"
+output_folder = "C:\\Users\\onepi\\OneDrive\\LogSync"
+
+# Maximum number of download retries
+max_retries = 3
 
 # Create the output and cache folders if they don't exist
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
-if not os.path.exists(cache_folder):
-    os.makedirs(cache_folder)
 
 mbox = mailbox.mbox(local_folders_path)
-for message in mbox:
+for index, message in enumerate(mbox):
     # Access email attributes
     subject = message['subject']
     from_address = message['from']
@@ -29,30 +31,43 @@ for message in mbox:
     # Process or save the email content as needed
     print(f"Subject: {subject}")
     print(f"From: {from_address}")
-    for index, part in enumerate(email_content):
-        decoded_html = quopri.decodestring(part.get_payload().encode('utf-8')).decode('utf-8')
+    
+    for retry in range(max_retries):
+        try:
+            for part in email_content:
+                decoded_html = quopri.decodestring(part.get_payload().encode('utf-8')).decode('utf-8')
+                href_matches = re.findall(href_pattern, decoded_html)
+                for href in href_matches:
+                    if "kindle-content-requests-prod" in href:
+                        # Download the PDF
+                        url = unquote(href)
+                        parsed_url = urlparse(url)
+                        query_params = parse_qs(parsed_url.query)
+
+                        # Extract the direct link to the PDF file
+                        pdf_url = query_params.get('U', [''])[0].split("?")[0]
+                        pdf_filename = os.path.basename(pdf_url)
+                        
+                        output_path = os.path.join(output_folder, pdf_filename)
+                        
+                        # Use the original URL instead
+                        pdf_url = href
+                        response = requests.get(pdf_url)
+                        response.raise_for_status()
+                        with open(output_path, 'wb') as pdf_file:
+                            for chunk in response.iter_content(chunk_size=8192):
+                                if chunk:
+                                    pdf_file.write(chunk)
+
+                        print(f"Downloaded PDF Link (Part {index}): {pdf_url}")
+                        print(f"Saved as: {pdf_filename}")
+            break
         
-        href_matches = re.findall(href_pattern, decoded_html)
-        for href in href_matches:
-                if "kindle-content-requests-prod" in href:
-
-                    # Download the PDF
-                    url = unquote(href)
-                    parsed_url = urlparse(url)
-                    query_params = parse_qs(parsed_url.query)
-
-                    # Extract the direct link to the PDF file
-                    pdf_filename = query_params.get('U', [''])[0].split("?")[0]
-                    # pdf_filename = os.path.join(output_folder, os.path.basename(pdf_url))
-                    cache_path = os.path.join(cache_folder, pdf_filename)
-                    output_path = os.path.join(output_folder, pdf_filename)
-                    
-                    # Use the original URL instead
-                    pdf_url = href
-                    response = requests.get(pdf_url)
-                    with open(pdf_filename, 'wb') as pdf_file:
-                        pdf_file.write(response.content)
-                   
-                   
-                    print(f"Downloaded PDF Link (Part {index}): {pdf_url}")
-                    print(f"Saved as: {pdf_filename}")
+        except requests.exceptions.RequestException as e:
+            print(str(e))
+            continue
+                        
+    else:
+        print(f"Exceeded maximum number of download retries for PDF (Part {index}).")
+    
+    time.sleep(3)
