246b52ac3d933d4bda59ee6872bc71918df5e16e
duydl <duy.dole.00ece@gmail.com>
2023-11-04 22:48:18 +0900

    fix: skip email older than 7 days

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 import time
 import json
 from urllib.parse import unquote, urlparse, parse_qs
-
+from datetime import datetime, timedelta, timezone
 
 # Function to prompt the user for settings
 def get_user_settings():
@@ -28,70 +28,85 @@ def load_settings():
             json.dump(settings, file)
     return settings
 
-# Load or create settings
-settings = load_settings()
+# Function to check if an email is older than 7 days
+def is_email_old(email_date, days=7):
+    now = datetime.now(timezone.utc)
+    email_date = datetime.strptime(email_date, "%a, %d %b %Y %H:%M:%S %z").replace(tzinfo=timezone.utc)
+    print(now, email_date)
+    return (now - email_date) > timedelta(days=days)
 
-# Open the mbox file
+# Main function to process emails
+def process_emails():
+    # Load or create settings
+    settings = load_settings()
 
+    # Open the mbox file
+    href_pattern = r'href="([^"]*)"'
 
-href_pattern = r'href="([^"]*)"'
-# Path to your Thunderbird Local Folders
-email_path = settings["email_path"]
-output_folder = settings["output_folder"]
+    # Path to your Thunderbird Local Folders
+    email_path = settings["email_path"]
+    output_folder = settings["output_folder"]
 
-# Maximum number of download retries
-max_retries = settings["max_retries"]
+    # Maximum number of download retries
+    max_retries = settings["max_retries"]
 
+    mails = mailbox.mbox(email_path)
 
-mailbox = mailbox.mbox(email_path)
-for index, message in enumerate(mailbox):
-    # Access email attributes
-    subject = message['subject']
-    from_address = message['from']
-    
-    # Access email content
-    email_content = message.get_payload()[0]
-    
-    # Process or save the email content as needed
-    print(f"Subject: {subject}")
-    print(f"From: {from_address}")
-    
-    for retry in range(max_retries):
-        try:
-            decoded_html = quopri.decodestring(email_content.get_payload().encode('utf-8')).decode('utf-8')
-            href = re.findall(href_pattern, decoded_html)
-            href = [link for link in href if "kindle-content-requests-prod" in link][0]
-            
-
-            # Download the PDF
-            url = unquote(href)
-            parsed_url = urlparse(url)
-            query_params = parse_qs(parsed_url.query)
-
-            # Extract the direct link to the PDF file
-            pdf_url = query_params.get('U', [''])[0].split("?")[0]
-            pdf_filename = os.path.basename(pdf_url)
-            
-            output_path = os.path.join(output_folder, pdf_filename)
-            
-            # Use the original URL instead
-            pdf_url = href
-            response = requests.get(pdf_url)
-            response.raise_for_status()
-            with open(output_path, 'wb') as pdf_file:
-                for chunk in response.iter_content(chunk_size=8192):
-                    if chunk:
-                        pdf_file.write(chunk)
-
-            print(f"Downloaded PDF Link (Part {index}): {pdf_url}")
-            print(f"Saved as: {pdf_filename}")
-            break
+    for index, message in enumerate(mails):
+        email_date = message['Date']
         
-        except requests.exceptions.RequestException as e:
-            print(str(e))
+        # Check if the email is older than 7 days, and skip if it is
+        if is_email_old(email_date):
             continue
-                        
-    else:
-        print(f"Exceeded maximum number of download retries for PDF (Email No. {index}).")
+        
+        # Access email attributes
+        subject = message['subject']
+        from_address = message['from']
+        
+        # Access email content
+        email_content = message.get_payload()[0]
+        
+        # Process or save the email content as needed
+        print(f"Subject: {subject}")
+        print(f"From: {from_address}")
+        
+        for retry in range(max_retries):
+            try:
+                decoded_html = quopri.decodestring(email_content.get_payload().encode('utf-8')).decode('utf-8')
+                href = re.findall(href_pattern, decoded_html)
+                href = [link for link in href if "kindle-content-requests-prod" in link][0]
 
-    time.sleep(5)
+                # Download the PDF
+                url = unquote(href)
+                parsed_url = urlparse(url)
+                query_params = parse_qs(parsed_url.query)
+
+                # Extract the direct link to the PDF file
+                pdf_url = query_params.get('U', [''])[0].split("?")[0]
+                pdf_filename = os.path.basename(pdf_url)
+                
+                output_path = os.path.join(output_folder, pdf_filename)
+                
+                # Use the original URL instead
+                pdf_url = href
+                response = requests.get(pdf_url)
+                response.raise_for_status()
+                with open(output_path, 'wb') as pdf_file:
+                    for chunk in response.iter_content(chunk_size=8192):
+                        if chunk:
+                            pdf_file.write(chunk)
+
+                # print(f"Downloaded PDF Link (Email No. {index}): {pdf_url}")
+                print(f"Saved as: {pdf_filename}")
+                break
+            
+            except requests.exceptions.RequestException as e:
+                print(str(e))
+                time.sleep(5)
+                continue
+        else:
+            print(f"Exceeded maximum number of download retries for PDF (Email No. {index}).")
+        time.sleep(5)
+        
+if __name__ == "__main__":
+    process_emails()
